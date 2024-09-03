b6267b0f966cdca220d08e2803ec53a7f534bde6
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-09-03 16:56:12 +0600

    Node JS

---
+++ modules/app-2.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = "John";
+const Tanjim = "Tanjim";
+
+module.exports = {john, Tanjim}
---
+++ modules/app.js
@@ -0,0 +1,8 @@
+const names = require("./app-2.js");
+const sayHi = require("./utils.js");
+const data = require("./alternative-syntex.js");
+require('./alternative-2.js')
+// console.log(data);
+// sayHi("Jack");
+// sayHi(names.john);
+// sayHi(names.Tanjim);
